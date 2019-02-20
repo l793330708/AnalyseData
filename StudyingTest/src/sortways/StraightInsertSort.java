3d393ca60f96e59e00bfad8f3f0d8438f9a39271
leiyingpeng <793330708@qq.com>
2019-02-20 13:53:35 +0800

    改变使用arraycopy方法

--- StudyingTest/src/sortways/StraightInsertSort.java
+++ StudyingTest/src/sortways/StraightInsertSort.java
@@ -1,7 +1,10 @@
 package sortways;
 
 public class StraightInsertSort {
-
+/**
+ * 直接插入排序
+ * @param args
+ */
 	public static void main(String[] args) {
 		int[] nums = new int[7];
 		nums[0] = 10;
@@ -12,16 +15,28 @@ public class StraightInsertSort {
 		nums[5] = 1;
 		nums[6] = 0;
 		
-		insertSort(nums);
+		insertSort(nums,2);
 		for(int num :nums) {
 			System.out.println(num);
 		}
 
 	}
+	public static void insertSort(int[] nums,int sort) {
+		
+		 nums = (sort==1) ? insertSort(nums):insertSort1(nums);
+	}
+	/**
+	 * 
+	 * @param nums 排序数组
+	 * @param desc 1为降序，2为升序
+	 * @return
+	 */
 	public static int[] insertSort(int[] nums) {
-		int count = 1;
-		for(int i = 1;i< nums.length ; i++) {
+		System.out.println("降序");
+		//int count = 1;
+		for(int i = 1;i < nums.length ; i++) {
 			//判断无序区第一元素和有序区最大元素大小。满足调用，不满足直接略过不更改位置
+			
 			if(nums[i]>nums[i-1]) {
 				int temp = nums[i];
 				int j = 0;
@@ -30,13 +45,39 @@ public class StraightInsertSort {
 				for(j = i -1 ;j>=0&&temp>nums[j];j--) {
 					//nums[j+1] =nums[j];
 				}
-				System.arraycopy(nums, j+1, nums, i-1,i);
-				System.out.println(j+"第"+(count-1)+"次交换");
+				System.arraycopy(nums, j+1, nums, j+2,i-1-j);
+				
 				
 				nums[j+1] =temp;
 			}
-			System.out.println("第"+count+"次排序");
-			count++;
+		
+			
+		}
+		return nums;
+	}
+	
+	public static int[] insertSort1(int[] nums) {
+		System.out.println("升序");
+		//int count = 1;
+		for(int i = 1;i < nums.length ; i++) {
+			//判断无序区第一元素和有序区最大元素大小。满足调用，不满足直接略过不更改位置
+			
+			if(nums[i]<nums[i-1]) {
+				int temp = nums[i];
+				int j = 0;
+				//保存,将有序区的向后移动
+				 //System.arraycopy(nums, i-1, nums, i, nums.length-i);
+				for(j = i -1 ;j>=0&&temp<nums[j];j--) {
+					//nums[j+1] =nums[j];
+				}
+				System.arraycopy(nums, j+1, nums, j+2,i-1-j);
+				//System.arraycopy(nums, j+1, nums, i-1,i-1);
+				
+				
+				nums[j+1] =temp;
+			}
+			
+			
 		}
 		return nums;
 	}
